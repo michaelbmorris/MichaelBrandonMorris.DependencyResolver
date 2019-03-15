36f7fd1c9323ffad4ef351439e8d15fbd2b42fb7
Michael Brandon Morris <mmorris@courtcall.com>
2019-03-15 00:43:11 -0400

    Initial commit

---
+++ MichaelBrandonMorris.DependencyResolver/DependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MichaelBrandonMorris.DependencyResolver
+{
+    /// <inheritdoc />
+    public class DependencyResolver : IDependencyResolver
+    {
+        public DependencyResolver(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        private IServiceProvider ServiceProvider { get; }
+
+        /// <inheritdoc />
+        public object GetService(Type serviceType)
+        {
+            return ServiceProvider.GetService(serviceType);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return ServiceProvider.GetServices(serviceType);
+        }
+    }
+}
